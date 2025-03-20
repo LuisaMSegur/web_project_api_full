f55ac546927474abc95f4c483b21e044a8168493
LuisaMSegur <165593500+LuisaMSegur@users.noreply.github.com>
2025-03-19 22:18:19 -0300

    vite configurar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
-export default {
-  define: {
-    'process.env': {},
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
